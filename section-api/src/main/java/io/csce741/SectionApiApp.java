e24723661c3ab29f6368d06c82ecb958b8f2a1a3
honorato88 <felipehonorato88@gmail.com>
2017-09-15 10:11:05 -0400

    1

---
+++ section-api/src/main/java/io/csce741/SectionApiApp.java
@@ -0,0 +1,13 @@
+package io.csce741;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SectionApiApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SectionApiApp.class, args);
+	}
+
+}
---
+++ section-api/src/main/java/io/csce741/controller/SectionController.java
@@ -0,0 +1,10 @@
+package io.csce741.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SectionController {
+
+	
+	
+}
