454e5f4f1f6099a6936f9117014bb1644ec90598
honorato88 <felipehonorato88@gmail.com>
2017-09-16 09:15:50 -0400

    1

--- section-api/src/main/java/io/csce741/SectionApiApp.java
+++ section-api/src/main/java/io/csce741/SectionApiApp.java
@@ -1,13 +1,20 @@
 package io.csce741;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.csce741.controller.Extractor;
+
 @SpringBootApplication
 public class SectionApiApp {
-
 	public static void main(String[] args) {
+		try {
+			Extractor.extractData();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		SpringApplication.run(SectionApiApp.class, args);
 	}
-
 }
--- section-api/src/main/java/io/csce741/controller/SectionController.java
+++ section-api/src/main/java/io/csce741/controller/SectionController.java
@@ -1,10 +1,34 @@
 package io.csce741.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.csce741.entities.Section;
 
 @RestController
 public class SectionController {
-
 	
+	@Autowired
+	private SectionService sectionService;
 	
+	@RequestMapping("/sections")
+	public List<Section> getAllSections() {
+		//return Extractor.sections;
+		return sectionService.getAllSections();
+	}
+	
+	@RequestMapping("/sections/{id}")
+	public Section getSection(@PathVariable String id) {
+		return sectionService.getSection(id);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, value="/sections")
+	public void addSection(Section section) {
+		sectionService.addSection(section);
+	}
 }
